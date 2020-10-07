16978de2ba68d69eea6fc28e22208216f0d31531
rileyseaburg <riley@DESKTOP-MQ1N2PR.localdomain>
2020-10-07 13:46:38 -0400

    configure stripe api and finish plugin.

--- controllers/stripe.js
+++ controllers/stripe.js
@@ -1,5 +1,5 @@
-'use strict';
-
+"use strict";
+const unitialzedStipe = require("stripe");
 /**
  * stripe.js controller
  *
@@ -7,7 +7,6 @@
  */
 
 module.exports = {
-
   /**
    * Default action.
    *
@@ -19,7 +18,7 @@ module.exports = {
 
     // Send 200 `ok`
     ctx.send({
-      message: 'ok'
+      message: "ok",
     });
   },
 
@@ -27,43 +26,68 @@ module.exports = {
    *  Add Stripe Settings Controller
    */
   updateSettings: async (ctx) => {
-    const { user } = ctx.state
-    const { pk } = ctx.request.body
+    const { user } = ctx.state;
+    const { pk } = ctx.request.body;
     // Ensure user is admin
     if (user.roles[0].id != 1) {
-      return ctx.unauthorized("Only administrators allowed.")
+      return ctx.unauthorized("Only administrators allowed.");
     }
     if (!pk) {
-      return ctx.throw(400, "Please provide a private key.")
+      return ctx.throw(400, "Please provide a private key.");
     }
     const pluginStore = strapi.store({
       environment: strapi.config.environment,
-      type: 'plugin',
-      name: 'stripe' // Recomment prefixing hammersport-stripe
-    })
-    const result = await pluginStore.set({ key: 'pk', value: pk })
+      type: "plugin",
+      name: "stripe", // Recomment prefixing hammersport-stripe
+    });
+    const result = await pluginStore.set({ key: "pk", value: pk });
     ctx.send({
-      result
-    })
+      result,
+    });
   },
 
-  retrieveSettings : async (ctx) => {
-    const {user} = ctx.state
+  retrieveSettings: async (ctx) => {
+    const { user } = ctx.state;
 
     if (user.roles[0].id != 1) {
-      return ctx.unauthorized("Only administrators allowed.")
+      return ctx.unauthorized("Only administrators allowed.");
     }
     const pluginStore = strapi.store({
       environment: strapi.config.environment,
-      type: 'plugin',
-      name: 'stripe'
-    })
+      type: "plugin",
+      name: "stripe",
+    });
 
-    const pk = await pluginStore.get({key: 'pk'})
+    const pk = await pluginStore.get({ key: "pk" });
 
     ctx.send({
-      pk: pk ? pk: ''
-    })
-  }
-};
+      pk: pk ? pk : "",
+    });
+  },
 
+  createPaymentIntent: async (ctx) => {
+    let { amount } = ctx.request.body;
+    amount = parseInt(amount);
+
+    if (isNaN(amount) || amount === 0) {
+      return ctx.throw(400, "Please provide a valid endpoint");
+    }
+
+    const pluginStore = strapi.store({
+      environment: strapi.config.environment,
+      type: "plugin",
+      name: "stripe",
+    });
+
+    const pk = await pluginStore.get({ key: "pk" });
+
+    const stripe = unitialzedStipe(pk);
+
+    const paymentIntent = await stripe.paymentIntents.create({
+      amount,
+      currency: "usd",
+    });
+
+    ctx.send({paymentIntent})
+  },
+};
